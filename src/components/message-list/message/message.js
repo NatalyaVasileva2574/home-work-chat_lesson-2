dfd9742f9aaf3b0453723f5c47286c188d48d9e2
NatalyaVasileva2574 <n2574@mail.ru>
2022-08-27 13:16:50 +0300

    add

--- src/components/message-list/message/message.js
+++ src/components/message-list/message/message.js
@@ -1,15 +1,15 @@
-import classNames from "classNames";
-import styles from "./message.module.css";
+// import classNames from "classnames";
+// import styles from "./message.module.css";
 
-export function Message({ message }) {
-  return (
-    <div className={classNames(styles.message, {
-      [styles.currentMessage]: message.author === "userName",
-    })}>
-      <h3>{message.message}</h3>
-      <p>{message.author}</p>
-      <p>12.03</p>
-    </div>
-  );
-}
+// export function Message({ message }) {
+//   return (
+//     <div className={classNames(styles.message, {
+//       [styles.currentMessage]: message.author === "userName",
+//     })}>
+//       <h3>{message.message}</h3>
+//       <p>{message.author}</p>
+//       <p>12.03</p>
+//     </div>
+//   );
+// }
 
