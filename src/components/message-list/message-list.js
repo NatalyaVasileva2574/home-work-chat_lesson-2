0e15ae6dfb85b7e71635de098d44224d8f9e348b
NatalyaVasileva2574 <n2574@mail.ru>
2022-08-27 13:32:50 +0300

    add

--- src/components/message-list/message-list.js
+++ src/components/message-list/message-list.js
@@ -1,3 +1,65 @@
+// import React, { useState, useEffect } from "react";
+// import { Message } from "./message";
+
+// export const MessageList = () => {
+//   const [messageList, setMessageList] = useState([]);
+//   const [value, setValue] = useState("");
+
+
+//   const sendMessage = () => {
+//     if (value) {
+//       setMessageList([
+//         ...messageList,
+//         { author: "userName", message: value },
+//       ]);
+//       setValue("");
+//     }
+//   };
+
+//   const handlePressInput = ({ code }) => {
+//     if (code === "Enter") {
+//       sendMessage();
+//     }
+//   };
+
+//   useEffect(() => {
+//     const lastMessage = messageList[messageList.length - 1];
+//     let timeId = null;
+
+//     if (messageList.length && lastMessage.author === "userName") {
+//       timeId = setTimeout(() => {
+//         setMessageList([
+//           ...MessageList,
+//           { autor: "Bot", message: "hello from Bot" }
+//         ]);
+//       }, 2500);
+
+//       return () => {
+//         clearInterval(timeId);
+//       };
+//     }
+//   }, [messageList]);
+
+//   return (
+//     <>
+//       <div>
+//         {messageList.map((message) => (
+//           <Message message={message} />
+//           ))}
+//       </div>
+
+//       <button onClick={sendMessage}>Send</button>
+//       <input fullWidth
+//         placeholder="введите текст"
+//         value={value}
+//         onChange={(e) => setValue(e.target.value)}
+//         onKeyPress={handlePressInput}
+//       />
+//     </>
+//   );
+// };
+
+
 import React, { useState, useEffect } from "react";
 import { Message } from "./message";
 
@@ -5,12 +67,11 @@ export const MessageList = () => {
   const [messageList, setMessageList] = useState([]);
   const [value, setValue] = useState("");
 
-
   const sendMessage = () => {
     if (value) {
       setMessageList([
         ...messageList,
-        { author: "userName", message: value },
+        { author: "User", message: value, date: new Date() },
       ]);
       setValue("");
     }
@@ -24,18 +85,18 @@ export const MessageList = () => {
 
   useEffect(() => {
     const lastMessage = messageList[messageList.length - 1];
-    let timeId = null;
+    let timerId = null;
 
-    if (messageList.length && lastMessage.author === "userName") {
-      timeId = setTimeout(() => {
+    if (messageList.length && lastMessage.author === "User") {
+      timerId = setTimeout(() => {
         setMessageList([
-          ...MessageList,
-          { autor: "Bot", message: "hello from Bot" }
+          ...messageList,
+          { author: "Bot", message: "Hello from Bot", date: new Date() },
         ]);
       }, 2500);
 
       return () => {
-        clearInterval(timeId);
+        clearInterval(timerId);
       };
     }
   }, [messageList]);
@@ -45,12 +106,13 @@ export const MessageList = () => {
       <div>
         {messageList.map((message) => (
           <Message message={message} />
-          ))}
+        ))}
       </div>
 
-      <button onClick={sendMessage}>Send</button>
-      <input fullWidth
-        placeholder="введите текст"
+      <button onClick={sendMessage}>send</button>
+      <input
+        fullWidth
+        placeholder="Введите сообщение..."
         value={value}
         onChange={(e) => setValue(e.target.value)}
         onKeyPress={handlePressInput}
