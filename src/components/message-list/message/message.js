6bd93a49fe401cc777570c4c8f64c0333ee1af0b
NatalyaVasileva2574 <n2574@mail.ru>
2022-08-27 12:52:03 +0300

    добавлены изменения

--- src/components/message-list/message/message.js
+++ src/components/message-list/message/message.js
@@ -1,10 +1,10 @@
-export function Message({message}){
-  return(
-    <div>
-      <h3>{message.message}</h3>
-      <p>{message.author}</p>
-      <p>12.03</p>
-    </div>
-  );
-}
+// export function Message({message}){
+//   return(
+//     <div>
+//       <h3>{message.message}</h3>
+//       <p>{message.author}</p>
+//       <p>12.03</p>
+//     </div>
+//   );
+// }
 
