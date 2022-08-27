59a0eda4f2e3585fd57c9ecb7832e4543fc4e61b
NatalyaVasileva2574 <n2574@mail.ru>
2022-08-27 13:37:29 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 // import React from 'react';
 // import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
 // import { FunctionComponent, ClassComponent } from './example'
-import styles from "./index.module.css";
-import {MessageList} from './components';
+// import styles from "./index.module.css";
+import { MessageList } from './components';
+import ReactDOM from 'react-dom/client';
 
 
-const Message2 = ({ message }) => {
-  return (
-    <div className={styles.test}>
-      <h1 className={styles.fontSizeNameClassComponent}>Message</h1>
-      <p className={styles.fontSizeText}>{message}</p>
-    </div>
-  );
-};
-
-const message = "Hello, react!!!";
-
-// const App2 = () => {
-//   return <Message2 message={message} />;
-// };
-
-
-
-// function FunctionComponent() {
-//   const [messageList, setMessageList] = useState([
-//     { text: "textMessage", author: "userName" }]);
-
-//   const [value, setValue] = useState("");
-
-//   return (
-//     <div>
-//       <input className={styles.formPadding} value={value} onChange={e => setValue(e.target.value)} />
-//     </div >
-//   );
-// }
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -45,9 +16,7 @@ const App = () => {
 
 root.render(
   // <React.StrictMode>
-    // <App2 />
-    <App />
-    // {/* <FunctionComponent /> */}
+  <App />
   // </React.StrictMode>
 );
 
